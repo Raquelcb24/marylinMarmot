33b6590be23a2b74a987715b7014d9d6ea90dc43
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-07-26 19:32:34 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-
 require('dotenv').config();
 
 const server = express();
-server.use(cors());
+
 server.use(express.json({ limit: '25mb' }));
+
+const corsOptions = {
+    origin: '*', // Permite todos los orígenes, ajusta según sea necesario
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+};
+
+// Aplicar el middleware de CORS con las opciones definidas
+server.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,14 +23,19 @@ server.listen(PORT, () => {
 });
 
 async function conexion() {
-    const conex = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    });
-    console.log('Conexion con la BD ' + conex.threadId);
-    return conex;
+    try {
+        const conex = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME
+        });
+        console.log('Conexion con la BD ' + conex.threadId);
+        return conex;
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+        throw new Error('No se pudo conectar con la base de datos');
+    }
 }
 
 server.get('/', (req, res) => {
@@ -62,7 +75,7 @@ server.get('/jewels', async (req, res) => {
         }
     } catch (error) {
         console.error('Error fetching jewels:', error);
-        res.status(500).json({ message: "Error interno del servidor" });
+        res.status(500).json({ message: "Error interno del servidor", error: error.message });
     }
 });
 
@@ -81,7 +94,7 @@ server.get('/collections/:id', async (req, res) => {
         }
     } catch (error) {
         console.error('Error fetching collection:', error);
-        res.status(500).json({ message: "Error interno del servidor" });
+        res.status(500).json({ message: "Error interno del servidor", error: error.message });
     }
 });
 
@@ -100,6 +113,6 @@ server.get('/jewels/:id', async (req, res) => {
         }
     } catch (error) {
         console.error('Error fetching jewel:', error);
-        res.status(500).json({ message: "Error interno del servidor" });
+        res.status(500).json({ message: "Error interno del servidor", error: error.message });
     }
 });
